202d4c6635a9670e9630bd121dac63df405c69f2
Joao.Silva <jl95terceira@gmail.com>
2025-06-04 20:21:26 +0100

    relax strict list methods

--- src/main/java/jl95/lang/StrictList.java
+++ src/main/java/jl95/lang/StrictList.java
@@ -17,13 +17,13 @@ public interface StrictList<T> extends I<T> {
     default boolean isEmpty() {
         return size() == 0;
     }
-    default boolean containsAll(Collection<? extends T> c) {
+    default boolean containsAll(Iterable<? extends T> c) {
         for (var c_: c) {
             if (!contains(c_)) return false;
         }
         return true;
     }
-    default boolean addAll(Collection<? extends T> c) {
+    default boolean addAll(Iterable<? extends T> c) {
         var changed = false;
         for (var c_: c) {
             if (!contains(c_)) {
@@ -33,22 +33,20 @@ public interface StrictList<T> extends I<T> {
         }
         return changed;
     }
-    default boolean retainAll(Collection<? extends T> c) {
+    default boolean retainAll(Iterable<? extends T> c) {
         var changed = false;
-        var selfCopy = I.of(this).toSet(); // cannot remove from set while iterating on it - need copy
-        for (var x: selfCopy) {
-            if (!c.contains(x)) {
+        for (var x: c) {
+            if (!contains(x)) {
                 changed = true;
                 remove(x);
             }
         }
         return changed;
     }
-    default boolean removeAll(Collection<? extends T> c) {
+    default boolean removeAll(Iterable<? extends T> c) {
         var changed = false;
-        var selfCopy = I.of(this).toSet(); // cannot remove from set while iterating on it - need copy
-        for (var x: selfCopy) {
-            if (c.contains(x)) {
+        for (var x: c) {
+            if (contains(x)) {
                 changed = true;
                 remove(x);
             }
